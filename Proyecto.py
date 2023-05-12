efc850b6e2abc65f0548af9676c9d27b92524e0d
Xxerix <a01425330>
2023-05-11 18:40:11 -0600

    2da Parte del Proyecto

--- Proyecto.py
+++ Proyecto.py
@@ -1,6 +1,7 @@
 #Integrantes: Edgar Olalde, Erika Garcia y Axel Godoy
 #Descripcion: Este programa es un chat que se conecta a un servidor MQTT y permite a los usuarios comunicarse entre si
 
+
 #Importaciones
 import threading
 import paho.mqtt.client as mqtt
@@ -18,3 +19,16 @@ def on_connect(client, userdata, flags, rc): # Se conecta al servidor
     print("Conectado con resultado:", rc) # Muestra el resultado de la conexion
     client.subscribe(topic) # Se suscribe al topico
     client.publish(topic, f"{nombre} ha entrado al chat") # Publica el mensaje de entrada al chat
+
+def on_message(client, userdata, msg): # Se conecta al servidor
+    print(msg.topic, str(msg.payload)) # Muestra el topico y el mensaje
+
+
+def receive_messages(client): # Recibe los mensajes
+    client.loop_forever() # Se mantiene en un loop infinito
+
+client = mqtt.Client() # Se crea el cliente
+client.on_connect = on_connect # Se conecta al servidor
+client.on_message = on_message # Se conecta al servidor con el mensaje
+
+client.connect("44.203.147.25", port, 60) # Se conecta al servidor con la ip y el puerto.
